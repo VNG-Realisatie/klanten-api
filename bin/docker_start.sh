132e19f1bd632bedfc887176d38cc0fb3eff04bb
Steven Bal <steven@maykinmedia.nl>
2020-08-31 11:04:26 +0200

    Use pg_isready instead of wait_for_db.sh in docker

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -2,12 +2,21 @@
 
 set -ex
 
+# Wait for the database container
+# See: https://docs.docker.com/compose/startup-order/
+export PGHOST=${DB_HOST:-db}
+export PGPORT=${DB_PORT:-5432}
+
 fixtures_dir=${FIXTURES_DIR:-/app/fixtures}
 
 uwsgi_port=${UWSGI_PORT:-8000}
 
-# Wait for required services
-${SCRIPTPATH}/wait_for_db.sh
+until pg_isready; do
+  >&2 echo "Waiting for database connection..."
+  sleep 1
+done
+
+>&2 echo "Database is up."
 
 # Apply database migrations
 >&2 echo "Apply database migrations"
